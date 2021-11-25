c0940bf961f87e77c294f72c1c0896b5d5cf577a
Mohamed Saleem <saleemilahi21@gmail.com>
2021-11-25 11:10:49 +0530

    Added static files

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ const DATABASE_URL = 'mongodb://localhost:27017'
 // Database connection
 connectDB(DATABASE_URL)
 
+// Middleware 
+app.use(express.urlencoded({extended:false}))
+
 // Static Files
 app.use('/student',express.static(join(process.cwd(),'public')))
+app.use('/student/update',express.static(join(process.cwd(),'public')))
 
 // Setting EJS Template
 app.set('view engine','ejs')
