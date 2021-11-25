307a6df90db62a50437ba4113d4cea95513d7c71
Mohamed Saleem <saleemilahi21@gmail.com>
2021-11-25 11:08:09 +0530

    modified Router and controller logic

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -1,16 +1,64 @@
 
+const studentModel = require('../model/student_Schema.js')
+
 class studentController {
 
-    static getAllDoc = (req,res) => {
-        res.render('index',{title: 'Create Student Details'})
+    static createDocPage = (req, res) => {
+        res.render('index', { title: 'Create Student Details' })
     }
 
-    static updateDoc = (req,res) => {
-        res.render('update',{title: 'Update Student Details'})
+    static createDoc = async (req, res) => {
+        try {
+            const {name,course,percent} = req.body
+            const doc = new studentModel({
+                name,
+                course,
+                percent
+            })
+            // Saving Document
+            await doc.save()
+
+            res.redirect('/student/view')
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    static studentView = (req,res) => {
-        res.render('student',{title: 'Student Details'})
+
+    static updateDoc = async (req, res) => {
+        try {
+            await studentModel.findByIdAndUpdate(req.params.id,req.body)
+            res.redirect('/student/view')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static updateDocPage = async (req, res) => {
+        try {
+            const result = await studentModel.findById(req.params.id)
+            res.render('update', { title: 'Update Student Details',data: result })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static deleteDoc = async (req, res) => {
+        try {
+            await studentModel.findByIdAndDelete(req.params.id)
+            res.redirect('/student/view')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static getAllDoc = async (req, res) => {
+        try {
+            const result = await studentModel.find()
+            res.render('student', { title: 'Student Details', data: result })
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
--- routes/student_route.js
+++ routes/student_route.js
@@ -3,8 +3,11 @@ const express  = require('express')
 const studentController = require('../controllers/student_controller.js')
 const router = express.Router()
 
-router.get('/',studentController.getAllDoc)
-router.get('/update',studentController.updateDoc)
-router.get('/view',studentController.studentView)
+router.get('/',studentController.createDocPage)
+router.post('/',studentController.createDoc)
+router.get('/update/:id',studentController.updateDocPage)
+router.post('/update/:id',studentController.updateDoc)
+router.post('/delete/:id',studentController.deleteDoc)
+router.get('/view',studentController.getAllDoc)
 
 module.exports = router
